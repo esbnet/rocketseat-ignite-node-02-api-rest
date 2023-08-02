5a73f59e4198747721d74c4b83d98177090502a9
esbnet <esbnet@gmail.com>
2023-08-02 16:15:07 -0300

    test: :sparkles: create add and list transaction test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,5 +1,5 @@
 import request from 'supertest'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { app } from '../src/app'
 
 describe('Transactions', () => {
@@ -12,14 +12,38 @@ describe('Transactions', () => {
     await app.close()
   })
   
-  it('should be able to create a new transaction', async () => {
-    const response = await request(app.server)
+  it.only('should be able to list all transaction', async () => {
+    await request(app.server)
       .post('/transactions')
       .send({
         title: 'Test',
         amount: 100,
         type: 'credit',
       }).expect(201)
+  }) 
+
+  it.only('should be able to create a new transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Test',
+        amount: 100,
+        type: 'credit',
+      })
+
+      const cookies = createTransactionResponse.get('Set-Cookie')
+      
+      const listTransactionResponse = await request(app.server)
+        .get('/transactions')
+        .set('Cookie', cookies)
+        .expect(200)
+
+        expect(listTransactionResponse.body.transactions).toEqual([
+          expect.objectContaining({
+            title: 'Test',
+            amount: 100
+          })
+        ])
   }) 
   
 })
